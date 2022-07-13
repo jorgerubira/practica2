7ee660d1c0603c7282d04969ebe8568f3805c3c2
JorgeRubira <jorgesub@gmail.com>
2022-07-13 17:59:04 +0200

    Merge origin/main into main
    Conflicts:
    	src/main/resources/application_caridad.properties

--- src/main/java/com/example/p2_app_tunombre/P2Controller.java
+++ src/main/java/com/example/p2_app_tunombre/P2Controller.java
@@ -8,6 +8,7 @@ package com.example.p2_app_tunombre;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class P2Controller {
+    
+    @Autowired
+    private Environment environment;    
     
     @Value(value = "${alumno}")
     private String alumno;
@@ -29,6 +33,9 @@ public class P2Controller {
     public String listado(Model m){
         m.addAttribute("alumno", alumno);
         m.addAttribute("port", port);
+        if (environment.getActiveProfiles().length>0){
+            m.addAttribute("profile", environment.getActiveProfiles()[0]);
+        }
         return "listado";
     }
     
