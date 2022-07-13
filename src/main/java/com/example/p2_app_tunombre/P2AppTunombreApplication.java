8f3afcfadb400ffd9491816af9604e5a6974ee43
JorgeRubira <jorgesub@gmail.com>
2022-07-13 17:47:55 +0200

    Merge origin/main into main
    Conflicts:
    	src/main/resources/application_caridad.properties

---
+++ src/main/java/com/example/p2_app_tunombre/P2AppTunombreApplication.java
@@ -0,0 +1,13 @@
+package com.example.p2_app_tunombre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2AppTunombreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2AppTunombreApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/crearBaseDatos.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE personas;
+
+USE personas;
+
+CREATE TABLE personas(id int primary key auto_increment, nombre varchar(200));
+
+
